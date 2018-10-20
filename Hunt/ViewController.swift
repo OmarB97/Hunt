0ba93ce5e19789ab09be1664e857c6d3f61e3901
Omar Baradei <obaradei@uber.com>
2018-10-20 15:07:06 -0400

    Initial Commit

---
+++ Hunt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hunt
+//
+//  Created by Omar Baradei on 10/20/18.
+//  Copyright © 2018 Omar Baradei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
